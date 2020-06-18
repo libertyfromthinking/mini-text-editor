7ea123610a34f700704de564b671b3c5a85cc9ab
greycells <libertyfromthinking@gmail.com>
2020-06-19 04:59:17 +0900

    redo undo add

--- mylib.js
+++ mylib.js
@@ -13,19 +13,21 @@ window.onload = function(){
 
 
 var words = document.getElementById('words')
-var add = document.getElementById('add')
-var undo = document.getElementById('undo')
-var redo = document.getElementById('redo')
+var addBtn = document.getElementById('add')
+var undoBtn = document.getElementById('undo')
+var redoBtn = document.getElementById('redo')
 var added = document.getElementById('added')
 var close = document.getElementsByClassName('close')
 
 // 선택된 단어들을 tagname:boolean 형식으로 저장, 추가 버튼을 눌러 우측 컴포넌트에 추가된 단어는 False, 추가예정인 단어는 True
 var selectedWords = {}
-
+var undoStack = Array()
+var redoStack = Array()
 
 // 추가 버튼 이벤트리스너
-add.addEventListener('click', addWords)
-
+addBtn.addEventListener('click', addWords)
+undoBtn.addEventListener('click', undo)
+redoBtn.addEventListener('click', redo)
 
 // length 5의 랜덤문자열 생성
 function randomString() {
@@ -44,28 +46,31 @@ function changeToRed(){
 	if (this.style.color=="red"){
 		this.style.color=""
 		delete selectedWords[this.className]
+		
 	} else if(this.style.color==""){
 		this.style.color="red"
 		selectedWords[this.className] = true
+		
 	}
+	// undoStack.push(this.className)
 	
 	
 }
 
 function addWords(){
 	for(var i in selectedWords){
-		if(selectedWords[i]){
-			el = document.getElementsByClassName(i)[0]
-			el.style.color = 'green'
-			el_cp = el.cloneNode(true)
-			el_cp.innerHTML += '<button class="close">X</button><br/>'
-			added.appendChild(el_cp)
-			selectedWords[i] = false
-		}
+		el = document.getElementsByClassName(i)[0]
+		el.style.color = 'green'
+		el_cp = el.cloneNode(true)
+		el_cp.innerHTML += '<button class="close">X</button><br/>'
+		added.appendChild(el_cp)
+		
 	}
 	for(var i=0; i<close.length; i++){
 		close[i].addEventListener('click', removeWord)
 	}
+	undoStack.push(selectedWords)
+	selectedWords = {}
 }
 	
 
@@ -73,5 +78,34 @@ function removeWord(){
 	remove = document.getElementsByClassName(this.parentNode.className)
 	added.removeChild(remove[1])
 	remove[0].style.color = ""
+	
+}
+
+function undo(){
+	if(undoStack.length>0){
+		obj = undoStack.pop()
+		redoStack.push(obj)
+		
+		for(var i in obj){
+			remove = document.getElementsByClassName(i)[1].childNodes[1].click()
+			
+		}
+
+		// clsName = undoStack.pop()
+		// document.getElementsByClassName(clsName)[0].click()
+		// redoStack.push(undoStack.pop())
+	}
+}
+
+function redo(){
+	if(redoStack.length>0){
+		selectedWords_temp = selectedWords;
+		selectedWords = redoStack.pop()
+		addBtn.click()
+		
+		// clsName = redoStack.pop()
+		// document.getElementsByClassName(clsName)[0].click()
+	}
+	
 	
 }
